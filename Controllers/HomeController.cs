b6e6d5ee6af40162732f4447c6132d98bc34f677
thijsvtol <43065692+thijsvtol@users.noreply.github.com>
2020-08-13 22:55:42 +0200

    ranglijsten

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -53,6 +53,54 @@ namespace schaatswedstrijden.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Ranglijsten(string seizoen, string afstanden, string categorie, string thuisbaan, string geredenbaan, string optioneel, string type, string maxdeelnemers, string maxpunten)
+        {
+            string klasse;
+            string gender;
+            if(categorie[0] == 'H')
+            {
+                gender = "Male";
+            }
+            else
+            {
+                gender = "Female";
+            }
+
+            if(maxdeelnemers == null)
+            {
+                maxdeelnemers = "null";
+            }
+
+            if (maxpunten == null)
+            {
+                maxpunten = "null";
+            }
+
+            if(geredenbaan == "null")
+            {
+                geredenbaan = null;
+            }
+
+            if (afstanden == "100" || afstanden == "300" || afstanden == "100%2B300" || afstanden == "100%2B500" || afstanden == "100%2B300%2B500"){
+                klasse = "100";
+            }
+            else{
+                klasse = "500";
+            }
+
+            string bestandsnaam = afstanden + " " + categorie + " " + seizoen;
+            ViewBag.Link = "https://www.emandovantage.com/api/reports/rankings/KNSB/SpeedSkating.LongTrack/SpeedSkating.LongTrack.PairsDistance.Individual/"+type+"?distanceValues="+afstanden+"&classificationWeight="+klasse+"&optionalColumns="+optioneel+"&cultureName=nl-NL&genders="+gender+"&categories="+categorie+"&countries=NED&homeVenues="+thuisbaan+"&venues="+geredenbaan+"&fixedSeason="+seizoen+"&count="+maxdeelnemers+"&maxPoints="+maxpunten+"&fileName="+bestandsnaam;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Ranglijsten()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
