c3c1de54e31741f859a97736d6b424f0eb169fc0
thijsvtol <43065692+thijsvtol@users.noreply.github.com>
2020-08-17 23:02:52 +0200

    fix onload season results & distance combination Controller & Model

---
+++ Controllers/DistanceCombinationController.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using schaatswedstrijden.Models;
+
+namespace schaatswedstrijden.Controllers
+{
+    public class DistanceCombinationController
+    {
+        public List<DistanceCombination> GetDistanceCombinations(string id)
+        {
+            WebRequest request = WebRequest.Create("https://knsbregistrations.azurewebsites.net/api/competitions/" + id + "/distancecombinations");
+            WebResponse response = request.GetResponse();
+            List<DistanceCombination> distanceCombinations;
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                distanceCombinations = JsonConvert.DeserializeObject<List<DistanceCombination>>(responseFromServer);
+            }
+
+            distanceCombinations = GetDistanceCombinationsSettings(id, distanceCombinations);
+
+            return distanceCombinations;
+        }
+
+        public List<DistanceCombination> GetDistanceCombinationsSettings(string id, List<DistanceCombination> distanceCombinations)
+        {
+            WebRequest request = WebRequest.Create("https://knsbregistrations.azurewebsites.net/api/competitions/" + id + "/settings/distancecombinations");
+            WebResponse response = request.GetResponse();
+            List<DistanceCombination> distanceCombinationsNew;
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                distanceCombinationsNew = JsonConvert.DeserializeObject<List<DistanceCombination>>(responseFromServer);
+            }
+
+            foreach (DistanceCombination distanceCombination in distanceCombinations)
+            {
+                foreach (DistanceCombination distanceCombinationNew in distanceCombinationsNew)
+                {
+                    if(distanceCombination.id == distanceCombinationNew.distanceCombinationId)
+                    {
+                        distanceCombination.isClosed = distanceCombinationNew.isClosed;
+                        distanceCombination.maxCompetitors = distanceCombinationNew.maxCompetitors;
+                        distanceCombination.requireVenueSubscription = distanceCombinationNew.requireVenueSubscription;
+                        distanceCombination.limitTimeDistanceValue = distanceCombinationNew.limitTimeDistanceValue;
+                        distanceCombination.limitTime = distanceCombinationNew.limitTime;
+                        distanceCombination.clubCodeFilter = distanceCombinationNew.clubCodeFilter;
+                        distanceCombination.homeVenueFilter = distanceCombinationNew.homeVenueFilter;
+                    }
+                }
+            }
+
+            distanceCombinations = GetCompetitionSettings(id, distanceCombinations);
+
+            return distanceCombinations;
+        }
+
+        public List<DistanceCombination> GetCompetitionSettings(string id, List<DistanceCombination> distanceCombinations)
+        {
+            WebRequest request = WebRequest.Create("https://knsbregistrations.azurewebsites.net/api/competitions/" + id + "/settings");
+            WebResponse response = request.GetResponse();
+            DistanceCombinationSettings distanceCombinationSettings;
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                distanceCombinationSettings = JsonConvert.DeserializeObject<DistanceCombinationSettings>(responseFromServer);
+            }
+
+            foreach (DistanceCombination distanceCombination in distanceCombinations)
+            {
+                foreach (DistanceCombination distanceCombinationNew in distanceCombinationSettings.distanceCombinations)
+                {
+                    if (distanceCombination.id == distanceCombinationNew.distanceCombinationId)
+                    {
+                        distanceCombination.opens = distanceCombinationNew.opens;
+                        distanceCombination.closes = distanceCombinationNew.closes;
+                        distanceCombination.price = distanceCombinationNew.price;
+                    }
+                }
+            }
+
+            return distanceCombinations;
+        }
+    }
+}
--- Models/DistanceCombination.cs
+++ Models/DistanceCombination.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace schaatswedstrijden.Models
 {
@@ -9,17 +7,28 @@ namespace schaatswedstrijden.Models
     public class DistanceCombination
     {
         public string id { get; set; }
+        public string distanceCombinationId { get; set; }
         public int number { get; set; }
         public string name { get; set; }
-        public object classFilter { get; set; }
+        public string classFilter { get; set; }
         public string categoryFilter { get; set; }
         public int classificationWeight { get; set; }
         public List<Distance> distances { get; set; }
-        public object starts { get; set; }
+        public string starts { get; set; }
         public int competitorsTotal { get; set; }
         public int competitorsPending { get; set; }
         public int competitorsConfirmed { get; set; }
         public int competitorsWithdrawn { get; set; }
+        public string opens { get; set; }
+        public string closes { get; set; }
+        public string price { get; set; }
+        public int maxCompetitors { get; set; }
+        public bool requireVenueSubscription { get; set; }
+        public string limitTimeDistanceValue { get; set; }
+        public string limitTime { get; set; }
+        public string clubCodeFilter { get; set; }
+        public string homeVenueFilter { get; set; }
+        public bool isClosed { get; set; }
     }
 
     public class Distance
@@ -31,5 +40,28 @@ namespace schaatswedstrijden.Models
         public int valueQuantity { get; set; }
         public string name { get; set; }
         public DateTime starts { get; set; }
+    }
+
+    public class DistanceCombinationSettings
+    {
+        public DateTime opens { get; set; }
+        public DateTime closes { get; set; }
+        public DateTime withdrawUntil { get; set; }
+        public bool isClosed { get; set; }
+        public bool isRegularOpen { get; set; }
+        public bool isLateOpen { get; set; }
+        public int maxCompetitors { get; set; }
+        public int competitorListGrouping { get; set; }
+        public bool allowMultipleCombinations { get; set; }
+        public List<DistanceCombination> distanceCombinations { get; set; }
+        public string paymentProvider { get; set; }
+        public string paymentProviderKey { get; set; }
+        public string paymentReference { get; set; }
+        public string extra { get; set; }
+        public string currency { get; set; }
+        public Contact contact { get; set; }
+        public string invitationIntroduction { get; set; }
+        public string invitationFooter { get; set; }
+        public string temporaryLicenseSecret { get; set; }
     }
 }
